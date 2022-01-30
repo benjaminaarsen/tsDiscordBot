32f473af39a48f00f815bdf8532d2b43bad38d58
benjaminaarsen <benjaminaarsen@outlook.com>
2022-01-30 23:13:19 +0100

    soundcloud token gen

--- src/test.js
+++ src/test.js
@@ -1,7 +1,3 @@
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-const link = "https://www.youtube.com/watch?v=j0lN0w5HVT8";
-
-
-ytdl(link)
-    .pipe(fs.createWriteStream('video.mp4'));
+const youtube = require("play-dl");
+// youtube.getFreeClientID().then((clientID) => {console.log(clientID)});
+youtube.authorization();
